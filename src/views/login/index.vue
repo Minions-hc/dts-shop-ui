736806b21b792ce64915ec4980f1ef493b1a044c
SywLby <syw_lby@163.com>
2025-05-05 21:17:56 +0800

    modify login page

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,10 +1,24 @@
 <template>
   <div class="login">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form">
-      <h3 class="title">聚惠星管理系统</h3>
+    <el-form
+      ref="loginForm"
+      :model="loginForm"
+      :rules="loginRules"
+      class="login-form"
+    >
+      <h3 class="title">Chill赏管理系统</h3>
       <el-form-item prop="username">
-        <el-input v-model="loginForm.username" type="text" auto-complete="off" placeholder="账号">
-          <svg-icon slot="prefix" icon-class="user" class="el-input__icon input-icon" />
+        <el-input
+          v-model="loginForm.username"
+          type="text"
+          auto-complete="off"
+          placeholder="账号"
+        >
+          <svg-icon
+            slot="prefix"
+            icon-class="user"
+            class="el-input__icon input-icon"
+          />
         </el-input>
       </el-form-item>
       <el-form-item prop="password">
@@ -15,7 +29,11 @@
           placeholder="密码"
           @keyup.enter.native="handleLogin"
         >
-          <svg-icon slot="prefix" icon-class="password" class="el-input__icon input-icon" />
+          <svg-icon
+            slot="prefix"
+            icon-class="password"
+            class="el-input__icon input-icon"
+          />
         </el-input>
       </el-form-item>
       <el-form-item prop="code">
@@ -26,19 +44,33 @@
           style="width: 63%"
           @keyup.enter.native="handleLogin"
         >
-          <svg-icon slot="prefix" icon-class="validCode" class="el-input__icon input-icon" />
+          <svg-icon
+            slot="prefix"
+            icon-class="validCode"
+            class="el-input__icon input-icon"
+          />
         </el-input>
-        <div class="login-code">
+
+        <div class="login-code">        <!-- 直接绑定base64字符串 -->
+          <img
+            :src="base64Image"
+            alt="验证码图片"
+            @click="refreshCaptcha"
+          >
         </div>
       </el-form-item>
-      <el-checkbox v-model="loginForm.rememberMe" style="margin:0px 0px 25px 0px;">记住密码</el-checkbox>
-      <el-form-item style="width:100%;">
+      <el-checkbox
+        v-model="loginForm.rememberMe"
+        style="margin: 0px 0px 25px 0px"
+      >记住密码</el-checkbox>
+      <el-form-item style="width: 100%">
         <el-button
           :loading="loading"
           size="medium"
           type="primary"
-          style="width:100%;"
-          @click.native.prevent="handleLogin">
+          style="width: 100%"
+          @click.native.prevent="handleLogin"
+        >
           <span v-if="!loading">登 录</span>
           <span v-else>登 录 中...</span>
         </el-button>
@@ -46,14 +78,14 @@
     </el-form>
     <!--  底部  -->
     <div class="el-login-footer">
-      <span>Copyright © 2020-2030 聚惠星版权所有.</span>
+      <span>Copyright © 2020-2030 Chill赏版权所有.</span>
     </div>
   </div>
 </template>
 
 <script>
-
 import Cookies from 'js-cookie'
+import axios from 'axios'
 
 export default {
   name: 'Login',
@@ -61,6 +93,7 @@ export default {
     return {
       codeUrl: '',
       cookiePassword: '',
+      base64Image: '',
       loginForm: {
         username: '',
         password: '',
@@ -75,7 +108,9 @@ export default {
         password: [
           { required: true, trigger: 'blur', message: '密码不能为空' }
         ],
-        code: [{ required: true, trigger: 'change', message: '验证码不能为空' }]
+        code: [
+          { required: true, trigger: 'change', message: '验证码不能为空' }
+        ]
       },
       loading: false,
       redirect: undefined
@@ -90,11 +125,10 @@ export default {
     }
   },
   created() {
-
     this.getCookie()
+    this.getCodeImage()
   },
   methods: {
-   
     getCookie() {
       const username = Cookies.get('username')
       const password = Cookies.get('password')
@@ -106,30 +140,58 @@ export default {
       }
     },
     handleLogin() {
-      this.$refs.loginForm.validate(valid => {
+      this.$refs.loginForm.validate((valid) => {
         if (valid) {
           this.loading = true
           if (this.loginForm.rememberMe) {
             Cookies.set('username', this.loginForm.username, { expires: 30 })
             Cookies.set('password', this.loginForm.password, { expires: 30 })
-            Cookies.set('rememberMe', this.loginForm.rememberMe, { expires: 30 })
+            Cookies.set('rememberMe', this.loginForm.rememberMe, {
+              expires: 30
+            })
           } else {
             Cookies.remove('username')
             Cookies.remove('password')
             Cookies.remove('rememberMe')
           }
-          this.$store.dispatch('LoginByUsername', this.loginForm).then(() => {
-            this.loading = false
-            this.$router.push({ path: this.redirect || '/' })
-          }).catch(response => {
-            this.$notify.error({
-              title: '失败',
-              message: response.data.errmsg
+          this.$store
+            .dispatch('LoginByUsername', this.loginForm)
+            .then(() => {
+              this.loading = false
+              this.$router.push({ path: this.redirect || '/' })
             })
-            this.loading = false
-          })
+            .catch((response) => {
+              this.$notify.error({
+                title: '失败',
+                message: response.data.errmsg
+              })
+              this.loading = false
+            })
         }
       })
+    },
+    async getCodeImage() {
+      try {
+        const url = process.env.VUE_APP_BASE_API + '/auth/captchaImage'
+        const response = await axios.get(url, {
+          responseType: 'blob' // 关键配置：声明响应类型为二进制流
+        })
+
+        // 转换Blob为Base64
+        const reader = new FileReader()
+        reader.onload = (e) => {
+          console.log(e.target.result)
+
+          this.base64Image = e.target.result
+        }
+        reader.readAsDataURL(response.data)
+      } catch (error) {
+        console.error('获取图片失败', error)
+      // 可在此处添加错误提示
+      }
+    },
+    refreshCaptcha() {
+      this.getCodeImage
     }
   }
 }
@@ -153,6 +215,7 @@ export default {
 .login-form {
   border-radius: 6px;
   background: #ffffff;
+  opacity: 0.9;
   width: 400px;
   padding: 25px 25px 5px 25px;
   .el-input {
@@ -180,6 +243,13 @@ export default {
     cursor: pointer;
     vertical-align: middle;
   }
+
+  .captcha-img {
+  height: 40px;
+  cursor: pointer;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+}
 }
 .el-login-footer {
   height: 40px;
@@ -188,7 +258,7 @@ export default {
   bottom: 0;
   width: 100%;
   text-align: center;
-  color: #fff;
+  color: #000;
   font-family: Arial;
   font-size: 12px;
   letter-spacing: 1px;
